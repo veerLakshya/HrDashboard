55e24eec58fa912a981798e3a03bfb75a74a1eee
Lakshya Veer Singh <109882583+veerLakshya@users.noreply.github.com>
2025-05-21 19:06:16 +0530

    added shadcn

--- app/components/sign-in-button.tsx
+++ app/components/sign-in-button.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { login } from "../lib/actions/auth";
-import { Button } from "@/components/ui/button";
+import { Button } from "@/app/components/ui/button";
 
 const SignInButton = () => {
   return (
